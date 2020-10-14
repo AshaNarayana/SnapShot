6f86a330ad3c3313bf1483a53c110b71365787eb
AshaNarayana <ashahnh@gmail.com>
2020-10-14 14:51:22 +0200

    Add dotenv file

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -9,7 +9,7 @@ const MapContainer = ({ longitude, latitude, title }) => {
     return (
       <div style={{ height: "50vh", width: "100%" }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyBarubOh2orvI1jq1T9F_9OZ7bcD_tqwer" }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
           defaultZoom={8}
           defaultCenter={{ lat: lat, lng: lon }}
         >
